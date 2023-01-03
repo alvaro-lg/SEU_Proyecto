1a970ecfcba36539c901d9684620f6bbdc629997
alvaro <alvaro.lopezgar@alumnos.upm.es>
2023-01-03 17:45:40 +0100

    Actualizado el .gitignore

---
+++ .modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/alvaro/Documents/SEU/Proyecto/modules.order := {   echo /home/alvaro/Documents/SEU/Proyecto/spkr-io.ko; :; } | awk '!x[$$0]++' - > /home/alvaro/Documents/SEU/Proyecto/modules.order
